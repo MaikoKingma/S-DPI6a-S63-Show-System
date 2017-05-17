c8f85b2cefd7c70438cf76e68f3790970a502db1
Maiko kingma <maiko.kingma@outlook.com>
2017-05-17 14:45:24 +0200

    Add Aggregation class to process outgoing Requests and incomming replies with the ShowAPIClient

---
+++ src/main/java/applications/ShowBroker/Aggregation.java
@@ -0,0 +1,55 @@
+package applications.ShowBroker;
+
+import shared.message.ShowAPIReply;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Maiko on 17-5-2017.
+ */
+public class Aggregation {
+    private int id;
+    private int expectedReplies;
+    private String corrolationId;
+    private List<ShowAPIReply> replies;
+
+    public Aggregation(int id, int expectedReplies, String corrolationId) {
+        this.expectedReplies = expectedReplies;
+        this.id = id;
+        this.corrolationId = corrolationId;
+        replies = new ArrayList<>();
+    }
+
+    public int getExpectedReplies() {
+        return expectedReplies;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCorrolationId() {
+        return corrolationId;
+    }
+
+    public int getReceivedReplies() {
+        return replies.size();
+    }
+
+    public void addBankInterestReply(ShowAPIReply reply) {
+        replies.add(reply);
+    }
+
+    public boolean repliesReceived() {
+        if (replies.size() == expectedReplies) {
+            return true;
+        }
+        return false;
+    }
+
+    public ShowAPIReply getBestReply() {
+        //ToDo
+        return null;
+    }
+}
