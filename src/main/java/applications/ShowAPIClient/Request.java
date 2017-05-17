402ca1ebfdf1adb2cad9a055c4d75d1fda613370
Maiko kingma <maiko.kingma@outlook.com>
2017-05-17 12:42:07 +0200

    Add request opbject to save correlation and aggregation ids with a request in the ShowAPIClient

---
+++ src/main/java/applications/ShowAPIClient/Request.java
@@ -0,0 +1,42 @@
+package applications.ShowAPIClient;
+
+import shared.message.ShowAPIRequest;
+
+/**
+ * Created by Maiko on 17-5-2017.
+ */
+public class Request {
+    private String corrolationId;
+    private ShowAPIRequest showAPIRequest;
+    private int aggregationId;
+
+    public Request(String corrolationId, ShowAPIRequest showAPIRequest, int aggregationId) {
+        this.corrolationId = corrolationId;
+        this.showAPIRequest = showAPIRequest;
+        this.aggregationId = aggregationId;
+    }
+
+    public String getCorrolationId() {
+        return corrolationId;
+    }
+
+    public void setCorrolationId(String corrolationId) {
+        this.corrolationId = corrolationId;
+    }
+
+    public ShowAPIRequest getShowAPIRequest() {
+        return showAPIRequest;
+    }
+
+    public void setShowAPIRequest(ShowAPIRequest showAPIRequest) {
+        this.showAPIRequest = showAPIRequest;
+    }
+
+    public int getAggregationId() {
+        return aggregationId;
+    }
+
+    public void setAggregationId(int aggregationId) {
+        this.aggregationId = aggregationId;
+    }
+}
