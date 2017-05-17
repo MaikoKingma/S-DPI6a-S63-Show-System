3ca18e65952d4ae875b576d5dff5f412a611370f
Maiko kingma <maiko.kingma@outlook.com>
2017-05-17 13:09:13 +0200

    Start implementing sendShowAPIReply

--- src/main/java/applications/ShowAPIClient/ShowBrokerGateway.java
+++ src/main/java/applications/ShowAPIClient/ShowBrokerGateway.java
@@ -1,19 +1,22 @@
 package applications.ShowAPIClient;
 
 import shared.gateway.*;
-import shared.message.ShowAPIReply;
+import shared.message.*;
 
 import javax.jms.*;
+import java.util.*;
 
 /**
  * Created by Maiko on 17-5-2017.
  */
 public class ShowBrokerGateway {
+    private List<Request> requests;
     private MessageSenderGateway sender;
     private MessageReceiverGateway receiver;
     private String name;
 
     public ShowBrokerGateway(String name) {
+        requests = new ArrayList<>();
         this.name = name;
         receiver = new MessageReceiverGateway(name + "RequestQueue");
 
@@ -23,7 +26,7 @@ public class ShowBrokerGateway {
                 try {
                     String body = ((TextMessage)m).getText();
                     System.out.println(">>> CorrelationId: " + m.getJMSCorrelationID() + " Message: " + body);
-                    OnShowAPIRequestArrived(((TextMessage)m).getText(), m.getJMSCorrelationID());
+                    OnShowAPIRequestArrived((TextMessage)m);
                 }
                 catch (JMSException e) {
                     e.printStackTrace();
@@ -32,11 +35,34 @@ public class ShowBrokerGateway {
         });
     }
 
-    public void SendShowAPIReply(ShowAPIReply reply) {
-        //ToDo
+    public void SendShowAPIReply(ShowAPIReply reply, ShowAPIRequest showAPIRequest) {
+        for (Request request : requests) {
+            if (request.getShowAPIRequest() == showAPIRequest)
+            {
+                try {
+                    Message message = sender.createTextMessage(reply, request.getCorrolationId());
+                    message.setIntProperty("aggregationId", request.getAggregationId());
+                    sender.send(message);
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
     }
 
-    public void OnShowAPIRequestArrived(String message, String correlationId) {
-        //ToDo
+    public void OnShowAPIRequestArrived(TextMessage message) {
+        try {
+            Integer aggregationId = message.getIntProperty("aggregationId");
+            if (aggregationId == null)
+                return;
+
+            ShowAPIRequest bankInterestRequest = new ShowAPIRequest();
+            //bankInterestRequest.fillFromCommaSeperatedValue(message.getText());
+            requests.add(new Request(message.getJMSCorrelationID(), bankInterestRequest, aggregationId));
+            //ToDo
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
     }
 }
