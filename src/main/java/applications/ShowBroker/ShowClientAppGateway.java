10c9e5ccc92907c132ca73cf594903eb9aa4b48e
Maiko kingma <maiko.kingma@outlook.com>
2017-05-17 14:50:52 +0200

    Add Send for ShowReplys and parse JSON to ShowRequests

--- src/main/java/applications/ShowBroker/ShowClientAppGateway.java
+++ src/main/java/applications/ShowBroker/ShowClientAppGateway.java
@@ -1,17 +1,22 @@
 package applications.ShowBroker;
 
+import com.google.gson.Gson;
 import shared.gateway.*;
 import shared.message.ShowReply;
+import shared.message.ShowRequest;
 
 import javax.jms.*;
+import java.util.*;
 
 /**
  * Created by Maiko on 12-5-2017.
  */
 public class ShowClientAppGateway {
     private MessageReceiverGateway receiver;
+    private List<Request> showRequests;
 
     public ShowClientAppGateway() {
+        showRequests = new ArrayList<>();
         receiver = new MessageReceiverGateway("showClientRequestQueue");
 
         receiver.setListener(m -> {
@@ -20,7 +25,7 @@ public class ShowClientAppGateway {
                 try {
                     String body = ((TextMessage)m).getText();
                     System.out.println(">>> CorrelationId: " + m.getJMSCorrelationID() + " Message: " + body);
-                    onShowRequestArrived(((TextMessage)m).getText(), m.getJMSCorrelationID());
+                    onShowRequestArrived(((TextMessage)m).getText(), m.getJMSCorrelationID(), m.getJMSReplyTo());
                 }
                 catch (JMSException e) {
                     e.printStackTrace();
@@ -29,11 +34,20 @@ public class ShowClientAppGateway {
         });
     }
 
-    public void onShowRequestArrived(String message, String correlationId) {
+    public void onShowRequestArrived(String json, String correlationId, Destination replyTo) {
+        ShowRequest request = new Gson().fromJson(json, ShowRequest.class);
+        showRequests.add(new Request(correlationId, request, replyTo));
         //ToDo
     }
 
-    public void sendShowReply(ShowReply reply) {
-        //ToDo
+    public void sendShowReply(ShowReply reply, String correlationId) {
+        for (Request request : showRequests) {
+            if (request.getCorrolationId().equals(correlationId)) {
+                MessageSenderGateway sender = new MessageSenderGateway(request.getReplyTo());
+                Message message = sender.createTextMessage(reply, correlationId);
+                sender.send(message);
+                break;
+            }
+        }
     }
 }
