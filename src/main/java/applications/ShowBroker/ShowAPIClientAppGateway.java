5ca9ca1c2809de63eff30f2fbe0f2bcd3d33f214
Maiko kingma <maiko.kingma@outlook.com>
2017-05-31 14:44:18 +0200

    Implemented ShowAPIClientGateway with scatter+gather patern

--- src/main/java/applications/ShowBroker/ShowAPIClientAppGateway.java
+++ src/main/java/applications/ShowBroker/ShowAPIClientAppGateway.java
@@ -1,18 +1,33 @@
 package applications.ShowBroker;
 
+import com.google.gson.Gson;
+import com.sun.istack.NotNull;
 import shared.gateway.MessageReceiverGateway;
-import shared.message.ShowAPIRequest;
+import shared.gateway.MessageSenderGateway;
+import shared.message.*;
 
 import javax.jms.*;
+import java.util.*;
 
 /**
  * Created by Maiko on 17-5-2017.
  */
 public class ShowAPIClientAppGateway {
+    private ShowBrokerManager manager;
     private MessageReceiverGateway receiver;
+    private List<Aggregation> aggregations;
+    @NotNull
+    private int id = 0;
+    private List<Rule> bankRules = new ArrayList<Rule>() {{
+        add(new Rule("Faker", false));
+    }};
 
-    public ShowAPIClientAppGateway() {
-        receiver = new MessageReceiverGateway("ShowAPIReplyQueue");
+    public ShowAPIClientAppGateway(ShowBrokerManager manager) {
+        aggregations = new ArrayList<>();
+        this.manager = manager;
+        receiver = new MessageReceiverGateway("ShowAPIReplyQueue");for (Rule rule : bankRules) {
+            rule.setSender(new MessageSenderGateway(rule.getApiName() + "RequestQueue"));
+        }
 
         receiver.setListener(m -> {
             if (m instanceof TextMessage)
@@ -20,7 +35,7 @@ public class ShowAPIClientAppGateway {
                 try {
                     String body = ((TextMessage)m).getText();
                     System.out.println(">>> CorrelationId: " + m.getJMSCorrelationID() + " Message: " + body);
-                    onShowRequestArrived(((TextMessage)m).getText(), m.getJMSCorrelationID());
+                    onShowAPIReplyArrived(((TextMessage)m).getText(), m.getJMSCorrelationID(), m.getIntProperty("aggregationId"));
                 }
                 catch (JMSException e) {
                     e.printStackTrace();
@@ -29,11 +44,50 @@ public class ShowAPIClientAppGateway {
         });
     }
 
-    private void onShowRequestArrived(String message, String correlationId) {
-        //ToDo
+    private void onShowAPIReplyArrived(String json, String correlationId, int aggregationId) {
+        ShowAPIReply reply = new Gson().fromJson(json, ShowAPIReply.class);
+        Aggregation foundAggregation = null;
+        for (Aggregation aggregation : aggregations) {
+            if (aggregation.getId() == aggregationId) {
+                foundAggregation = aggregation;
+                break;
+            }
+        }
+        if (foundAggregation != null) {
+            foundAggregation.addShowAPIReply(reply);
+            if (foundAggregation.repliesReceived()){
+                manager.processReply(foundAggregation.getBestReply(), correlationId);
+            }
+        }
     }
 
-    public void SendShowAPIRequest(ShowAPIRequest request) {
-        //ToDo
+    public void SendShowAPIRequest(ShowAPIRequest request, String correlationId) {
+        int aggregationId = getAggregationID();
+        int expectedReplies = 0;
+        List<Rule> rulesToSend = new ArrayList<>();
+        for (Rule rule : bankRules) {
+            if ((request.isGuess() && rule.hasSearch()) || !request.isGuess()) {
+                expectedReplies++;
+                rulesToSend.add(rule);
+            }
+        }
+        aggregations.add(new Aggregation(aggregationId, expectedReplies, correlationId));
+        for (Rule rule : rulesToSend) {
+            try {
+                MessageSenderGateway sender = rule.getSender();
+                Message message = sender.createTextMessage(request, correlationId);
+                message.setIntProperty("aggregationId", aggregationId);
+                sender.send(message);
+            } catch (JMSException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @NotNull
+    private int getAggregationID()
+    {
+        id++;
+        return id;
     }
 }
